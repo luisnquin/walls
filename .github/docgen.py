75a8405b489516557d0f7f16a3cf42fa61574360
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-06-13 22:50:35 -0600

    fix(docgen): fixed some pylint errors

--- .github/docgen.py
+++ .github/docgen.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+"""Generate README files for all project folders."""
 
 from typing import List
-from os import walk, getenv, path, remove
+from os import walk, getenv, path
 
 
 target_folders: List[str] = ['desktop']
@@ -14,38 +15,45 @@ def main():
     repo_branch = must_env('GITHUB_REPO_BRANCH')
 
     for folder_path in target_folders:
-        for subdirectory_info in walk(folder_path):
-            subdirectory_path = subdirectory_info[0]
-            subdirectory_files = subdirectory_info[2]
+        for subfolder_info in walk(folder_path):
+            subfolder_path = subfolder_info[0]
+            subfolder_files = subfolder_info[2]
 
-            if subdirectory_path == folder_path:
+            if subfolder_path == folder_path:
                 continue
 
             folder_info = get_folder_info(
-                repo_owner, repo_name, repo_branch, subdirectory_path, subdirectory_files)
+                repo_owner, repo_name, repo_branch, subfolder_path, subfolder_files)
 
-            readme_file_path = '/'.join(['.', folder_info.path, 'README.md'])
-
-            with open(readme_file_path, '+w') as file:
-                file.write('# HEADER\n')
-
-                for file_url in folder_info.url_files:
-                    image = '![{}]({})\n'.format(
-                        path.basename(file_url), file_url)
-
-                    file.write(image)
+            generate_readme_for_subfolder(folder_info)
 
 
 class FolderInfo:
+    """A class that represents information about a folder."""
+
     path: str
     url_files: List[str]
 
-    def __init__(self, path) -> None:
+    def __init__(self, folder_path: str) -> None:
         self.url_files = []
-        self.path = path
+        self.path = folder_path
 
 
-def get_folder_info(repo_owner: str, repo_name: str, repo_branch: str, subdirectory_path: str, subdirectory_files: List[str]) -> FolderInfo:
+def generate_readme_for_subfolder(folder_info: FolderInfo):
+    readme_file_path = '/'.join(['.', folder_info.path, 'README.md'])
+
+    with open(readme_file_path, '+w', encoding='utf-8') as file:
+        file.write('# HEADER\n')
+
+        for file_url in folder_info.url_files:
+            file_name_no_ext = path.splitext(path.basename(file_url))[0]
+            image = f'![{file_name_no_ext}]({file_url})\n'
+
+            file.write(image)
+
+
+def get_folder_info(repo_owner: str, repo_name: str, repo_branch: str,
+                    subdirectory_path: str, subdirectory_files: List[str]) -> FolderInfo:
     folder_info = FolderInfo(subdirectory_path)
 
     for file_name in subdirectory_files:
@@ -60,14 +68,27 @@ def get_folder_info(repo_owner: str, repo_name: str, repo_branch: str, subdirect
     return folder_info
 
 
+class EnvironmentVariableError(Exception):
+    """Exception raised for errors related to environment variables."""
+
+    def __init__(self, name: str, message: str):
+        self.name = name
+        self.message = message
+        super().__init__(self.message)
+
+
 def must_env(name) -> str:
+    """Retrieve the value of an environment variable."""
+
     value = getenv(name)
 
     match value:
         case None:
-            raise Exception(f'Environment variable {name} is not set')
+            raise EnvironmentVariableError(
+                name, f'Environment variable {name} is not set')
         case '':
-            raise Exception(f'Environment variable {name} is empty')
+            raise EnvironmentVariableError(
+                name, f'Environment variable {name} is not set')
 
     return value
 
